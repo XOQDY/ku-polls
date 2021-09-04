f19306fde2d12ce3e9a9a9122ebc5cc55470ef16
XOQDY <numfahmaek@gmail.com>
2021-09-04 17:38:55 +0700

    add detail.html and index.html file into polls/templete/polls/ and also add index, detail, results and vote page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,24 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Yo this is polls website if you don't know it btw. \U0001F618")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "Yep this is result of question %s \U0001F973."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("If you don't know now you're voting on question %s. \U0001F631" % question_id)
